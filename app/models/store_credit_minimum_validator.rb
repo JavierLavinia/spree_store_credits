4d7bd4def401ac9a5671334a86f122de2a4eb072
nando <fernando.garcia@the-cocktail.com>
2012-12-26 14:55:26 +0100

    minimum validation error message localized

--- app/models/store_credit_minimum_validator.rb
+++ app/models/store_credit_minimum_validator.rb
@@ -1,7 +1,9 @@
 class StoreCreditMinimumValidator < ActiveModel::Validator
   def validate(record)
-    if Spree::Config[:use_store_credit_minimum] and record.item_total < Spree::Config[:use_store_credit_minimum].to_f and record.store_credit_amount > 0
-      record.errors[:base] <<  "Order's item total is less than the minimum allowed (#{Spree::Config[:use_store_credit_minimum].to_f}) to use store credit."
+    if Spree::Config[:use_store_credit_minimum] and
+       record.item_total < Spree::Config[:use_store_credit_minimum].to_f and
+       record.store_credit_amount > 0
+      record.errors[:base] << I18n.t('less_than_minimum_validation', :minimum => Spree::Config[:use_store_credit_minimum].to_f)
     end
   end
 end
