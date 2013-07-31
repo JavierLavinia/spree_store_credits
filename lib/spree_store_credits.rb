8822618e1ad429a42838b26b0c70a348247c591a
Alvaro Sanchez <alvaro.sanchez@the-cocktail.com>
2013-07-31 16:07:08 +0200

    GiveStoreCredit no selectable from promo adjustmens

--- lib/spree_store_credits.rb
+++ lib/spree_store_credits.rb
@@ -16,7 +16,7 @@ module SpreeStoreCredits
     end
 
     initializer 'spree_store_credits.register.promotion.actions', :after => 'spree.promo.register.promotions.actions' do |app|
-      app.config.spree.promotions.actions <<  Spree::Promotion::Actions::GiveStoreCredit
+      #app.config.spree.promotions.actions <<  Spree::Promotion::Actions::GiveStoreCredit
     end
 
     config.to_prepare &method(:activate).to_proc
